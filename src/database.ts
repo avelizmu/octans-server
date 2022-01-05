00d1594e67835754e2430511037ddc4724a870cd
Adrian Veliz <adrian.veliz@ecoprintq.com>
2022-01-04 22:10:56 -0500

    Add migrations to database

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,10 @@
-import { Kysely, MysqlDialect } from 'kysely'
+import {Kysely, Migration, MysqlDialect} from 'kysely'
 import {databaseConfig} from "../config/index.js";
+import path, {dirname} from "path";
+import {readdirSync} from "fs";
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 interface Database {
 }
@@ -7,5 +12,13 @@ interface Database {
 const database = new Kysely<Database>({
     dialect: new MysqlDialect(databaseConfig)
 })
+
+const migrationFiles = readdirSync(path.join(__dirname, 'migrations')).filter(fileName => fileName.endsWith('.js'));
+const migrations: {[key: string]: Migration} = {};
+for(let fileName of migrationFiles) {
+    migrations[fileName.substring(0, fileName.length - 2)] = (await import('file://' + path.join(__dirname, 'migrations', fileName)));
+}
+await database.migration.migrateToLatest(migrations);
+
 
 export default database;
